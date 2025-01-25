a4df7de8ccccaf210b970ec8f63ca055a03b4be1
bastress <andrew.bastress@gmail.com>
2025-01-25 15:14:30 -0500

    backend and frontend communication

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const LandingPage = () => {
+    const [schedule, setSchedule] = useState(null);
+
+    const fetchSchedule = async () => {
+        try {
+            const response = await axios.get("http://127.0.0.1:5000/schedule");
+            console.log("Backend Response:", response.data);
+            setSchedule(response.data);
+        } catch (error) {
+            console.error("Error fetching schedule:", error);
+        }
+    };
+    
+
+    useEffect(() => {
+        fetchSchedule();
+        console.log(schedule);
+    }, []);
+
     return (
-        <div></div>
+        <div>
+            {schedule ? (
+                <pre>{JSON.stringify(schedule, null, 2)}</pre>
+            ) : (
+                "Loading..."
+            )}
+        </div>
     );
 }
 
